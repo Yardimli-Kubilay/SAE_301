c0a254ac77c08bbb0ad7dddddaa95fbcd986bc42
Kubilay6688 <kubilay.yardimli@edu.univ-fcomte.fr>
2023-10-24 19:46:38 +0200

    ajout des svg et debut header

---
+++ sae_301/src/components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+  <header class="app-header">
+    <!-- Logo -->
+    <div class="logo">
+      <img src="@/assets/Logo_tavue.png" alt="TaVue Logo" />
+    </div>
+
+    <!-- Navigation -->
+    <nav class="nav-menu">
+      <router-link to="/">ACCUEIL</router-link>
+      <router-link to="/personnalisation">PERSONNALISATION</router-link>
+      <router-link to="/lunette">LUNETTE</router-link>
+    </nav>
+
+    <!-- Icons -->
+    <div class="icons">
+      <router-link to="/se-connecter">
+        <img src="@/components/icons/Icon_buste.svg" alt="Se connecter" />
+      </router-link>
+      <router-link>
+        <img src="@/components/icons/Icon_coeur.svg" alt="Enregistrer" />
+      </router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+.app-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #C1B488;
+  padding: 10px 20px;
+  height: 150px;
+  flex-shrink: 0;
+}
+
+.logo img {
+  height: 100px; /* Ajustez selon la taille de votre logo */
+}
+
+.nav-menu {
+  display: flex;
+  gap: 20px; /* Espace entre les éléments */ 
+}
+
+.nav-menu a {
+  text-decoration: none;
+  color: inherit;
+  font-size: 24px;
+}
+
+.icons {
+  display: flex;
+  gap: 15px; /* Espace entre les icônes */
+}
+
+.icons img {
+  height: 24px; /* Ajustez selon la taille de vos SVG */
+}
+</style>
